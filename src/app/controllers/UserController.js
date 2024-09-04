784f3fd8500defa4bbde7c78b82fec906a7da8dc
Marcos Torres <marcossttg-torres@hotmail.com>
2024-09-03 22:21:13 -0300

    Add files via upload

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,60 @@
+import * as Yup from 'yup'
+
+import { v4 } from 'uuid'
+
+import User from '../models/User'
+/*
+ Padrão MVC nos controles tem este padrão
+ store => Cadastra ou Adiciona
+ index =>  Listar vários
+ show => Listar apenas UM
+ update => Atualizar
+ delete => Deletar
+ */
+
+class UserController {
+
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(3),
+      admin: Yup.boolean(),
+    })
+
+    try {  // valida schema error do validatesync
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { name, email, password, admin } = request.body
+
+    // validação do email com findOne
+    const userExists = await User.findOne({
+      where: { email },
+    })
+
+    // validar se usuário existe
+    if (userExists) {
+      return response.status(409).json({ error: 'User already exists' })
+    }
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password,
+      admin,
+    })
+
+    return response.status(201).json({
+      id: user.id,
+      name,
+      email,
+      admin,
+    })
+  }
+}
+
+export default new UserController()
---
+++ src/routes.js
@@ -0,0 +1,42 @@
+import { Router } from 'express'
+
+import multer from 'multer'
+
+import multerConfig from './config/multer'
+
+import authMiddleware from './app/middlewares/auth'
+
+import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+import ProductController from './app/controllers/ProductController'
+import CategoryController from './app/controllers/CategoryController'
+import OrderController from './app/controllers/OrderController'
+
+const upload = multer(multerConfig)
+
+const routes = new Router()
+
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Welcome to the DEVBURGER-API' })
+})
+
+routes.post('/users', UserController.store)
+routes.post('/session', SessionController.store)
+
+// Adicionando routes.use() com middleware para que todas as rotas abaixo.
+// Quando chamado, recebe o middleware como autenticação.
+routes.use(authMiddleware)
+
+routes.post('/products', upload.single('file'), ProductController.store)
+routes.get('/products', ProductController.index)
+routes.put('/products/:id', upload.single('file'), ProductController.update)
+
+routes.post('/categories', upload.single('file'), CategoryController.store)
+routes.get('/categories', CategoryController.index)
+routes.put('/categories/:id', upload.single('file'), CategoryController.update)
+
+routes.post('/orders', OrderController.store)
+routes.get('/orders', OrderController.index)
+routes.put('/orders/:id', OrderController.update)
+
+export default routes
