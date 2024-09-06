4a143fba7b898792799700e48f1c85ba0b90db5d
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-09-06 00:30:01 -0300

    removed .dotenv

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,8 @@
-require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
   // url para utilizar com railway,
   url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
-
-  // utilizando .env
-  // host: process.env.PG_HOST,
-  // port: process.env.PG_PORT,
-  // username: process.env.PG_USERNAME,
-  // password: process.env.PG_PASSWORD,
-  // database: process.env.PG_DATABASE,
 
   define: {
     timestamps: true,
