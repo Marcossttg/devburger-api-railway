0a940b1f72672e49494134fcd95afedc95d025b7
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-09-06 00:14:48 -0300

    add port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from './app'
 
 const port = process.env.APP_PORT || 3001;
-app.listen(port, '0.0.0.0', () => console.log(`👨‍💻 App is running at port ${port}...`))
+app.listen(port, '0.0.0.0')
 
-// app.listen(port, '0.0.0.0')
+// app.listen(port, '0.0.0.0', () => console.log(`👨‍💻 App is running at port ${port}...`))
 // app.listen(3001)
