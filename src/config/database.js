c59c34f31303f27a11159bb5bd0da8d3234d3757
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-09-07 23:29:20 -0300

    restored to original

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
-const config = {
+
+module.exports = {
   dialect: 'postgres',
-  // URL para utilizar com Railway
+  // url para utilizar com railway,
   url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
 
   define: {
@@ -8,18 +9,4 @@ const config = {
     underscored: true,
     underscoredAll: true,
   },
-};
-
-export default config;
-
-// module.exports = {
-//   dialect: 'postgres',
-//   // url para utilizar com railway,
-//   url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
-
-//   define: {
-//     timestamps: true,
-//     underscored: true,
-//     underscoredAll: true,
-//   },
-// }
+}
