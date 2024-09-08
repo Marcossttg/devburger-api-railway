b2f4c7848dfd3a2dc75cb9f1ca4d1259241dced0
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-09-07 23:39:25 -0300

    changing to test database/seq/create-user

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,22 @@
-
-module.exports = {
+export default {
   dialect: 'postgres',
-  // url para utilizar com railway,
   url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
-
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
-}
+};
+
+
+// module.exports = {
+//   dialect: 'postgres',
+//   // url para utilizar com railway,
+//   url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
+
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     underscoredAll: true,
+//   },
+// }
--- src/database/migrations/20240723231840-create-users-table.js
+++ src/database/migrations/20240723231840-create-users-table.js
@@ -1,37 +1,38 @@
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
+import { DataTypes } from 'sequelize';
+
+export default {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
       id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
         allowNull: false,
         primaryKey: true,
       },
       name: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
       },
       email: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
         unique: true,
       },
       password_hash: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
       },
       admin: {
-        type: Sequelize.BOOLEAN,
+        type: DataTypes.BOOLEAN,
         defaultValue: false,
         allowNull: false,
       },
       createdAt: {
-        type: Sequelize.DATE,
+        type: DataTypes.DATE,
         allowNull: false,
       },
       updatedAt: {
-        type: Sequelize.DATE,
+        type: DataTypes.DATE,
         allowNull: false,
       },
     });
@@ -41,3 +42,48 @@ module.exports = {
     await queryInterface.dropTable('Users');
   },
 };
+
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.createTable('Users', {
+//       id: {
+//         type: Sequelize.UUID,
+//         defaultValue: Sequelize.UUIDV4,
+//         allowNull: false,
+//         primaryKey: true,
+//       },
+//       name: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//       },
+//       email: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//         unique: true,
+//       },
+//       password_hash: {
+//         type: Sequelize.STRING,
+//         allowNull: false,
+//       },
+//       admin: {
+//         type: Sequelize.BOOLEAN,
+//         defaultValue: false,
+//         allowNull: false,
+//       },
+//       createdAt: {
+//         type: Sequelize.DATE,
+//         allowNull: false,
+//       },
+//       updatedAt: {
+//         type: Sequelize.DATE,
+//         allowNull: false,
+//       },
+//     });
+//   },
+
+//   async down(queryInterface) {
+//     await queryInterface.dropTable('Users');
+//   },
+// };
