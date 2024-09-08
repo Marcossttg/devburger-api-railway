3dc8c1ca80026074fc8785110b8a8787632e2070
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-09-07 23:47:59 -0300

    return orig

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,12 @@
+
 module.exports = {
   dialect: 'postgres',
+  // url para utilizar com railway,
   url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
+
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
-};
-
-
-
-// module.exports = {
-//   dialect: 'postgres',
-//   // url para utilizar com railway,
-//   url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
-
-//   define: {
-//     timestamps: true,
-//     underscored: true,
-//     underscoredAll: true,
-//   },
-// }
+}
--- src/server.js
+++ src/server.js
@@ -2,3 +2,6 @@ import app from './app.js'
 
 const port = process.env.APP_PORT || 3001;
 app.listen(port, '0.0.0.0', () => console.log(`👨‍💻 App is running at port ${port}...`))
+
+// app.listen(port, '0.0.0.0', () => console.log(`👨‍💻 App is running at port ${port}...`))
+// app.listen(3001)
