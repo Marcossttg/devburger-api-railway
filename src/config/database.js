3f609263d3082ddbe2af91a36aad987ed2fed48b
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-09-07 23:19:52 -0300

    att syntax database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,6 @@
-
-module.exports = {
+const config = {
   dialect: 'postgres',
-  // url para utilizar com railway,
+  // URL para utilizar com Railway
   url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
 
   define: {
@@ -9,4 +8,18 @@ module.exports = {
     underscored: true,
     underscoredAll: true,
   },
-}
+};
+
+export default config;
+
+// module.exports = {
+//   dialect: 'postgres',
+//   // url para utilizar com railway,
+//   url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
+
+//   define: {
+//     timestamps: true,
+//     underscored: true,
+//     underscoredAll: true,
+//   },
+// }
