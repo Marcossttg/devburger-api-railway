550629368c3daf48dffe7d3325d62fb81fe4034f
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-09-10 00:00:19 -0300

    att

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,12 @@
 module.exports = {
   dialect: "postgres",
-  url: 'postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway',
+  url: "postgresql://postgres:WDFTqwfTFKZRPpJSYmEmPgkncVaWrnRG@junction.proxy.rlwy.net:22573/railway",
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
 };
-
-
-
 
 // require('dotenv').config();
 
--- src/database/index.js
+++ src/database/index.js
@@ -16,7 +16,9 @@ class Database {
   }
 
   init() {
-    this.connection = new Sequelize(configDatabase.url);
+    this.connection = new Sequelize(
+      "postgresql://postgres:NMgIdHlyVINWztvqrKeDXTeXaeWObJbD@junction.proxy.rlwy.net:16122/railway",
+    );
     models
       .map((model) => model.init(this.connection))
       .map(
@@ -26,7 +28,7 @@ class Database {
 
   mongo() {
     this.mongoConnection = mongoose.connect(
-      'mongodb://mongo:gqSdwrjWqmlChGBucQgrsjoBtpVKZslJ@junction.proxy.rlwy.net:30960',
+      "mongodb://mongo:gqSdwrjWqmlChGBucQgrsjoBtpVKZslJ@junction.proxy.rlwy.net:30960",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -37,6 +39,15 @@ class Database {
 
 export default new Database();
 
+// DEPOIS TROCAR
+// init() {
+//   this.connection = new Sequelize(configDatabase.url);
+//   models
+//     .map((model) => model.init(this.connection))
+//     .map(
+//       (model) => model.associate && model.associate(this.connection.models),
+//     );
+// }
 
 // import Sequelize from 'sequelize'
 
@@ -47,7 +58,6 @@ export default new Database();
 // import User from '../app/models/User'
 // import Product from '../app/models/Product'
 // import Category from '../app/models/Category'
-
 
 // const models = [User, Product, Category]
 
