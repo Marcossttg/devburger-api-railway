9fe31979314ddfcc9bc591a5bd958db78e954920
Marcossttg <102203599+Marcossttg@users.noreply.github.com>
2024-09-17 22:17:32 -0300

    test migrate

--- src/database/migrations/20240728214645-delete-category-column.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn("Products", "category");
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn("Products", "category", Sequelize.STRING);
-  },
-};
